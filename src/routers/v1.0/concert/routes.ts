ff8fc11313d11cf32c91beca6ba4ebad2bd3576f
yungblud <killi8n@gmail.com>
2023-08-06 10:09:32 +0900

    feat: added concert route

---
+++ src/routers/v1.0/concert/routes.ts
@@ -0,0 +1,16 @@
+import { FastifyPluginCallback } from 'fastify'
+
+const concertRoutes: FastifyPluginCallback = (instance, opts, done) => {
+//   instance.post(
+//     '/',
+//     {
+//         preValidation: (req, rep) => {
+
+//         },
+//     },
+//     () => {}
+//   )
+  done()
+}
+
+export default concertRoutes
--- src/routers/v1.0/routes.ts
+++ src/routers/v1.0/routes.ts
@@ -1,12 +1,13 @@
 import { FastifyPluginCallback } from 'fastify'
 import accountRoutes from './account/routes'
+import concertRoutes from './concert/routes'
 
 // api/v1
 const apiV1Routes: FastifyPluginCallback = (instance, opts, done) => {
   // account route
   instance.register(accountRoutes, { 'prefix': "/account" })
   // concert route
-//   instance.register(() => {}, { prefix: '/concert' })
+  instance.register(concertRoutes, { prefix: '/concert' })
   done()
 }
 
