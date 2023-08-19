73e7464599c9436f51efe7b8ff8e77b59f7f9a7e
yungblud <killi8n@gmail.com>
2023-08-19 11:19:03 +0900

    refactor: code relocation

---
+++ src/routers/v1.0/concert/handlers.ts
@@ -0,0 +1,48 @@
+import { RouteHandler, onRequestHookHandler } from "fastify"
+import { CreateConcertBodySchema, CreateConcertBodyType } from "./types"
+import { createConcert } from "../../../database/concert"
+import { decodeAuthToken } from "../../../lib/authToken"
+import { getUserById } from "../../../database/user"
+
+export const handleCreateConcert: RouteHandler<{
+    Body: CreateConcertBodyType
+  }> = async (req, rep) => {
+    const validation = CreateConcertBodySchema.safeParse(req.body)
+    if (!validation.success) {
+      return rep.status(400).send({
+        error: 'invalid data'
+      })
+    }
+    const concert = await createConcert(req.body)
+    return rep.send(concert.serialize())
+  }
+
+export const handleOnRequestCreateConcert: onRequestHookHandler = async (req, rep, done) => {
+    const { authorization } = req.headers
+    // check authorization header for token
+    if (!authorization) {
+      return rep.status(403).send({
+        error: "not authorized"
+      })
+    }
+    const authToken = authorization
+    const decoded = decodeAuthToken(authToken)
+    if (!decoded) {
+      return rep.status(400).send({
+        error: 'token decode error'
+      })
+    }
+    const user = await getUserById(decoded.id)
+    if (!user) {
+      return rep.status(401).send({
+        error: 'invalid token'
+      })
+    }
+    const isStaff = user.isStaffUesr()
+    if (!isStaff) {
+      return rep.status(403).send({
+        error: 'has no permission'
+      })
+    }
+    done()
+  }
--- src/routers/v1.0/concert/routes.ts
+++ src/routers/v1.0/concert/routes.ts
@@ -1,8 +1,6 @@
 import { FastifyPluginCallback } from 'fastify'
-import { decodeAuthToken } from '../../../lib/authToken'
-import { getUserById } from '../../../database/user'
-import { CreateConcertBodySchema, CreateConcertBodyType } from './types'
-import { createConcert } from '../../../database/concert'
+import { CreateConcertBodyType } from './types'
+import { handleCreateConcert, handleOnRequestCreateConcert } from './handlers'
 
 const concertRoutes: FastifyPluginCallback = (instance, opts, done) => {
   instance.route<{
@@ -10,45 +8,8 @@ const concertRoutes: FastifyPluginCallback = (instance, opts, done) => {
   }>({
     'method': 'POST',
     'url': '/',
-    onRequest: async (req, rep, done) => {
-      const { authorization } = req.headers
-      // check authorization header for token
-      if (!authorization) {
-        return rep.status(403).send({
-          error: "not authorized"
-        })
-      }
-      const authToken = authorization
-      const decoded = decodeAuthToken(authToken)
-      if (!decoded) {
-        return rep.status(400).send({
-          error: 'token decode error'
-        })
-      }
-      const user = await getUserById(decoded.id)
-      if (!user) {
-        return rep.status(401).send({
-          error: 'invalid token'
-        })
-      }
-      const isStaff = user.isStaffUesr()
-      if (!isStaff) {
-        return rep.status(403).send({
-          error: 'has no permission'
-        })
-      }
-      done()
-    },
-    handler: async (req, rep) => {
-      const validation = CreateConcertBodySchema.safeParse(req.body)
-      if (!validation.success) {
-        return rep.status(400).send({
-          error: 'invalid data'
-        })
-      }
-      const concert = await createConcert(req.body)
-      return rep.send(concert.serialize())
-    },
+    onRequest: handleOnRequestCreateConcert,
+    handler: handleCreateConcert,
     schema: {
       'tags': ['concert'],
       body: {
