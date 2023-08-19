9b731f16e5309e2177fc5b85696784cb86802432
yungblud <killi8n@gmail.com>
2023-08-19 17:26:40 +0900

    feat: modified auth token

--- src/lib/authToken.ts
+++ src/lib/authToken.ts
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import jwtDecode from "jwt-decode";
+import { addMinutes } from "date-fns";
 
 export interface JWTPayload {
   id: string;
@@ -11,16 +12,30 @@ dotenv.config();
 
 const { JWT_SECRET: jwtSecret } = process.env;
 
+const AUTH_TOKEN_ALIVE_MINUTES = 30;
+
 export function generateAuthToken(payload: JWTPayload): string {
   if (typeof jwtSecret === "undefined") {
     throw new Error("no secret");
   }
-  return jwt.sign(payload, jwtSecret);
+  const defaultIATDate = new Date();
+  return jwt.sign(
+    {
+      ...payload,
+      iat: Math.floor(defaultIATDate.getTime() / 1000),
+      exp: Math.floor(
+        addMinutes(defaultIATDate, AUTH_TOKEN_ALIVE_MINUTES).getTime() / 1000,
+      ),
+    },
+    jwtSecret,
+  );
 }
 
-export function decodeAuthToken(token: string): JWTPayload | null {
+export function decodeAuthToken(
+  token: string,
+): (JWTPayload & { iat: number; exp: number }) | null {
   try {
-    const decoded = jwtDecode<JWTPayload>(token);
+    const decoded = jwtDecode<JWTPayload & { iat: number; exp: number }>(token);
     return decoded;
   } catch (e) {
     return null;
